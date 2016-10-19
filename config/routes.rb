b2f5b48353bf904394bde42489ca541a130191f8
Bang Nguyen <bangnv77@gmail.com>
2016-10-19 16:55:02 -0700

    create and display the api for the first user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # API routes path
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-
+      resources :users, :only => [:show]
     end
   end
-end 
+end
